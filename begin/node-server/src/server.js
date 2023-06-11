37623e72ba9044e72ef6b7181d67a1ecd0b528a9
anhquan74-dev <anhquan74.dev@gmail.com>
2023-06-11 16:46:41 +0700

    template engine EJS

--- begin/node-server/src/server.js
+++ begin/node-server/src/server.js
@@ -1,6 +1,11 @@
 const express = require("express"); // commonjs
+const path = require("path");
 const app = express(); // app express
 const port = 8080; // port
+
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // routes
 // app.METHOD(PATH, HANDLER)
@@ -10,6 +15,10 @@ app.get("/", (req, res) => {
 
 app.get("/test1", (req, res) => {
   res.send("<h1>Test route 1</h1>");
+});
+
+app.get("/test-ejs", (req, res) => {
+  res.render("sample.ejs");
 });
 
 app.listen(port, () => {
