a2fedea5cde3bdc6eeb41f82be1d6e5ea8dca846
anhquan74-dev <anhquan74.dev@gmail.com>
2023-06-11 16:27:14 +0700

    more routes

--- begin/node-server/server.js
+++ begin/node-server/server.js
@@ -1,9 +1,15 @@
-const express = require("express");
-const app = express();
-const port = 8080;
+const express = require("express"); // commonjs
+const app = express(); // app express
+const port = 8080; // port
 
+// routes
+// app.METHOD(PATH, HANDLER)
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/test1", (req, res) => {
+  res.send("<h1>Test route 1</h1>");
 });
 
 app.listen(port, () => {
