c62ba0957651637889d9442459ae176a51300798
anhquan74-dev <anhquan74.dev@gmail.com>
2023-06-11 16:07:01 +0700

    helloworld with expressjs

--- begin/node-server/server.js
+++ begin/node-server/server.js
@@ -1,16 +1,11 @@
-const http = require("http"); // library - node.js
+const express = require("express");
+const app = express();
+const port = 8080;
 
-const hostname = "127.0.0.1"; // localhost
-const port = 3000; // port (0-65535)
-
-// create server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\nLearn NodeJS Backend");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// server address listen on: ` http://${hostname}:${port}/`
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
