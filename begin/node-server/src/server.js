f12dcbbc7d68cbd5361b868c6b3675b9c5440d08
anhquan74-dev <anhquan74.dev@gmail.com>
2023-06-12 17:44:19 +0700

    using static file

--- begin/node-server/src/server.js
+++ begin/node-server/src/server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT; // port
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// config static files
+app.use(express.static(path.join(__dirname, "public")));
+
 // routes
 // app.METHOD(PATH, HANDLER)
 app.get("/", (req, res) => {
