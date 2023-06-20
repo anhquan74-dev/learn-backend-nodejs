362c87f0e7cec36304a7dc104afa20a1ad9f6823
anhquan74-dev <anhquan74.dev@gmail.com>
2023-06-20 19:56:26 +0700

    connect mysql2

--- begin/node-server/src/server.js
+++ begin/node-server/src/server.js
@@ -3,6 +3,7 @@ const express = require("express"); // commonjs
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express(); // app express
 const hostname = process.env.HOST_NAME;
@@ -14,6 +15,21 @@ configViewEngine(app);
 // routes
 // app.METHOD(PATH, HANDLER)
 app.use("/", webRoutes);
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "learn-nodejs",
+});
+
+// simple query
+connection.query("SELECT * FROM `Users`", function (err, results, fields) {
+  console.log(results); // results contains rows returned by server
+  // console.log(fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
